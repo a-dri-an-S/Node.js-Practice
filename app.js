e28c7329043dbc1de7c6e5a3d39db1f78f9be48c
ADD RIAN <adrian.salinas.1986@gmail.com>
2022-02-11 14:27:49 -0800

    adding middleware

--- app.js
+++ app.js
@@ -5,6 +5,17 @@ const express = require('express');
 
 const app = express();
 
+// middleware
+app.use((req, res, next) => {
+    console.log('In the middleware!');
+    next();
+});
+
+app.use((req, res, next) => {
+    console.log('In another middleware!');
+    res.send('<h1>Hello, from express!</h1>')
+});
+
 // create server
 const server = http.createServer(app);
 
