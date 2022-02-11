97c8f5fe005624bd24572057d650388615c8df5e
ADD RIAN <adrian.salinas.1986@gmail.com>
2022-02-11 15:06:21 -0800

    handeling routes

--- app.js
+++ app.js
@@ -1,23 +1,24 @@
-const http = require('http');
-
 // importing express
 const express = require('express');
 
 const app = express();
 
 // middleware
-app.use((req, res, next) => {
-    console.log('In the middleware!');
+app.use('/', (req, res, next) => {
+    console.log('This always works!');
     next();
 });
 
-app.use((req, res, next) => {
+app.use('/add-product', (req, res, next) => {
     console.log('In another middleware!');
+    res.send('<h1>The "Add product" page!</h1>')
+});
+
+app.use('/', (req, res, next) => {
+    console.log('In a middleware!');
     res.send('<h1>Hello, from express!</h1>')
 });
 
 // create server
-const server = http.createServer(app);
-
-server.listen(3000);
+app.listen(3000)
 
