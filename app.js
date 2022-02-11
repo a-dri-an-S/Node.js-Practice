82a8dbd59f09dbc9a35630a1cdf0bcb280c5302d
ADD RIAN <adrian.salinas.1986@gmail.com>
2022-02-10 18:08:38 -0800

    write file

--- app.js
+++ app.js
@@ -23,15 +23,16 @@ const server = http.createServer((req, res) => {
             console.log(chunk);
             body.push(chunk);
         });
-        req.on('end', () => {
+        return req.on('end', () => {
             const parsedBody = Buffer.concat(body).toString();
             const message = parsedBody.split("=")[1];
-            fs.writeFileSync('message.txt', message);
+            // write file
+            fs.writeFile('message.txt', message, err => {
+                res.statusCode = 302;
+                res.setHeader('Location', '/');
+                return res.end();
+            });
         });
-        
-        res.statusCode = 302;
-        res.setHeader('Location', '/');
-        return res.end();
     }
     // sending a response
     res.setHeader('Content-Type', 'text/html');
