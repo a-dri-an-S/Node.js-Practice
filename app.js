fa0f04c1966c45ff16022fcc68b22edcdbeb09a6
ADD RIAN <adrian.salinas.1986@gmail.com>
2022-02-11 15:17:30 -0800

    parsing requests w/ body-parser

--- app.js
+++ app.js
@@ -1,21 +1,22 @@
-// importing express
+// imports
 const express = require('express');
+const bodyParser = require('body-parser');
 
 const app = express();
 
 // middleware
-app.use('/', (req, res, next) => {
-    console.log('This always works!');
-    next();
-});
+app.use(bodyParser.urlencoded({extended: false}));
 
 app.use('/add-product', (req, res, next) => {
-    console.log('In another middleware!');
-    res.send('<h1>The "Add product" page!</h1>')
+    res.send("<form action='/product' method='POST'><input type='text' name='title'><button type='submit'>App Product</button></input></form>");
 });
 
+app.use('/product', (req, res, next) => {
+    console.log(req.body);
+    res.redirect('/')
+})
+
 app.use('/', (req, res, next) => {
-    console.log('In a middleware!');
     res.send('<h1>Hello, from express!</h1>')
 });
 
