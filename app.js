ac31b676759616ca4fc5ba078447548215257ab2
ADD RIAN <adrian.salinas.1986@gmail.com>
2022-02-11 18:59:36 -0800

    Added 404 HTML page

--- app.js
+++ app.js
@@ -1,3 +1,6 @@
+// Node Imports
+const path = require('path');
+
 // imports
 const express = require('express');
 const bodyParser = require('body-parser');
@@ -14,9 +17,8 @@ app.use('/admin', adminRoutes);
 app.use(shopRoutes);
 
 app.use((req, res, next) => {
-    res.status(404).send("<h1>Page not found!</h1>")
+    res.status(404).sendFile(path.join(__dirname, 'views', '404.html'));
 })
-
 
 // create server
 app.listen(3000);
