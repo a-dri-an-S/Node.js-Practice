a39f4fd2a59716a100b65edf00a1ced9ea2c3472
ADD RIAN <adrian.salinas.1986@gmail.com>
2022-02-14 15:39:18 -0800

    Using partials for layouts w/ Ejs

--- routes/admin.js
+++ routes/admin.js
@@ -12,9 +12,9 @@ router.get('/add-product', (req, res, next) => {
     res.render('add-product', {
         pageTitle: "Add Product", 
         path:"/admin/add-product",
-        activeAddProduct: true,
-        productCSS: true,
-        formsCSS: true
+        // activeAddProduct: true,
+        // productCSS: true,
+        // formsCSS: true
     });
 });
 
