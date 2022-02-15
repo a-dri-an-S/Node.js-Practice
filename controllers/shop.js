7d4912950fc1719b40acc6cb3f62e8fa042fc758
ADD RIAN <adrian.salinas.1986@gmail.com>
2022-02-14 21:35:50 -0800

    added new input fields + updated product Model + updated admin controller

--- controllers/shop.js
+++ controllers/shop.js
@@ -1,15 +1,5 @@
 const Product = require('../models/product');
 
-exports.getProducts = (req, res, next) => {
-    Product.fetchAll((products) => {
-        res.render('shop/product-list', {
-            prods: products, 
-            pageTitle: 'All Products', 
-            path: "/products"
-        });
-    });
-}
-
 exports.getIndex = (req, res, next) => {
     Product.fetchAll((products) => {
         res.render('shop/index', {
@@ -18,18 +8,28 @@ exports.getIndex = (req, res, next) => {
             path: "/"
         });
     });
-}
+};
+
+exports.getProducts = (req, res, next) => {
+    Product.fetchAll((products) => {
+        res.render('shop/product-list', {
+            prods: products, 
+            pageTitle: 'All Products', 
+            path: "/products"
+        });
+    });
+};
 
 exports.getCart = (req, res, next) => {
     res.render('shop/cart', {
         path: "/cart",
         pageTitle: "You Cart"
     })
-}
+};
 
 exports.getCheckout = (req, res, next) => {
     res.render('shop/checkout', {
         path: "/checkout",
         pageTitle: "Checkout"
     })
-}
+};
