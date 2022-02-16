429ba54c13c8da4e2d9505577bfaad27c259469e
ADD RIAN <adrian.salinas.1986@gmail.com>
2022-02-16 15:57:46 -0800

    sync models to MYSQL db via Sequelize

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 
 const errorController = require('./controllers/error');
-const db = require('./util/database');
+const sequelize = require('./util/database');
 
 // configs
 // require('dotenv').config();
@@ -29,5 +29,11 @@ app.use(shopRoutes);
 
 app.use(errorController.get404);
 
-// create server
-app.listen(3000);
+sequelize.sync()
+    .then(result => {
+        // console.log(result);
+        app.listen(3000);
+    })
+    .catch(err => {
+        console.log(err);
+    })
