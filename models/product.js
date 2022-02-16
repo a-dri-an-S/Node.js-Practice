40505102fecfc4acff7ab772d7b9371a7aab8c89
ADD RIAN <adrian.salinas.1986@gmail.com>
2022-02-15 19:23:09 -0800

    updated Cart / Products model to delete products + controllers & routes

--- models/product.js
+++ models/product.js
@@ -1,6 +1,8 @@
 const fs = require('fs');
 const path = require('path');
 
+const Cart = require('./cart');
+
 const p = path.join(
     path.dirname(require.main.filename), 
     'data', 
@@ -25,6 +27,7 @@ module.exports = class Product {
         this.price = price;
 
     }
+
     save() {
         getProductsFromFile(products => {
             if (this.id) {
@@ -43,6 +46,18 @@ module.exports = class Product {
             }
 
         });
+    }
+
+    static deleteById(id) {
+        getProductsFromFile(products => {
+            const product = products.find(prod => prod.id === id);
+            const updatedProducts = products.filter(prod => prod.id !== id);
+            fs.writeFile(p, JSON.stringify(updatedProducts), err => {
+                if (!err) {
+                    Cart.deleteProduct(id, product.price);
+                }
+            });
+        })
     }
 
     static fetchAll(callback) {
