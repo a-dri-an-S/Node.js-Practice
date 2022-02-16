813e553cff42399be3e3fd5172d3fdee6b72fe0e
ADD RIAN <adrian.salinas.1986@gmail.com>
2022-02-16 15:38:43 -0800

    installed sequelize + connect to MYSQL db via sequelize

--- util/database.js
+++ util/database.js
@@ -1,12 +1,10 @@
-const mysql = require('mysql2');
+const Sequelize = require('sequelize');
 
 require('dotenv').config();
 
-const pool = mysql.createPool({
-    host: 'localhost',
-    user: 'root',
-    database: 'node',
-    password: process.env.DB_PASSWORD
+const sequelize = new Sequelize('node', 'root', process.env.DB_PASSWORD, {
+    dialect: 'mysql', 
+    host: 'localhost'
 });
 
-module.exports = pool.promise();
+module.exports = sequelize;
