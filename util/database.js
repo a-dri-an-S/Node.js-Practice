d25cb7223181f015255dfa6778bf113f7def3ee0
ADD RIAN <adrian.salinas.1986@gmail.com>
2022-02-16 23:41:04 -0800

    updated mongoDb connection

--- util/database.js
+++ util/database.js
@@ -1,17 +1,29 @@
 const mongodb = require('mongodb');
 const MongoClient = mongodb.MongoClient;
 
+let _db;
+
 require('dotenv').config();
 
 const mongoConnect = callback => {
     MongoClient.connect(process.env.DB_CONNECT)
         .then(client => {
-            console.log('connected!')
-            callback(client)
+            console.log('connected!');
+            _db = client.db();
+            callback();
         })
         .catch(err => {
             console.log(err);
+            throw err;
         })
+};
+
+const getDb = () => {
+    if (_db) {
+        return _db;
+    }
+    throw 'No database found!';
 }
 
-module.exports = mongoConnect;
+exports.mongoConnect = mongoConnect;
+exports.getDb = getDb;
