fdabc9a95b3fcce9de0a36183a424361f1e4aa4d
ADD RIAN <adrian.salinas.1986@gmail.com>
2022-02-16 18:59:49 -0800

    added User one-to-many Product realtion for MYSQL

--- app.js
+++ app.js
@@ -8,8 +8,8 @@ const bodyParser = require('body-parser');
 const errorController = require('./controllers/error');
 const sequelize = require('./util/database');
 
-// configs
-// require('dotenv').config();
+const Product = require('./models/product');
+const User = require('./models/user');
 
 const app = express();
 
@@ -29,7 +29,13 @@ app.use(shopRoutes);
 
 app.use(errorController.get404);
 
-sequelize.sync()
+Product.belongsTo(User, { 
+    constraints: true, 
+    onDelete: 'CASCADE' 
+});
+User.hasMany(Product);
+
+sequelize.sync({ force: true })
     .then(result => {
         // console.log(result);
         app.listen(3000);
