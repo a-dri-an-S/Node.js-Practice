4a9be99e0933c4c94db29c05daca474515de1ac5
ADD RIAN <adrian.salinas.1986@gmail.com>
2022-02-16 16:33:18 -0800

    retrieving data from MYSQL db via sequelize + render to view

--- controllers/shop.js
+++ controllers/shop.js
@@ -2,22 +2,24 @@ const Product = require('../models/product');
 const Cart = require('../models/cart');
 
 exports.getIndex = (req, res, next) => {
-    Product.fetchAll()
-        .then(([rows]) => {
+    Product.findAll()
+        .then(products => {
             res.render('shop/index', {
-                prods: rows, 
+                prods: products, 
                 pageTitle: 'Shop', 
                 path: "/"
             });
         })
-        .catch(err => console.log(err));
+        .catch(err => {
+            console.log(err);
+        });
 };
 
 exports.getProducts = (req, res, next) => {
-    Product.fetchAll()
-    .then(([rows]) => {
+    Product.findAll()
+    .then((products) => {
         res.render('shop/product-list', {
-            prods: rows, 
+            prods: products, 
             pageTitle: 'All Products', 
             path: "/products"
         });
