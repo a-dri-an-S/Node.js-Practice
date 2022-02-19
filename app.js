889cc020cf20798e307b08576ab34fe03f746a3e
ADD RIAN <adrian.salinas.1986@gmail.com>
2022-02-18 18:25:55 -0800

    added dependency connect-mongodb-session + configured, save session to mongodb

--- app.js
+++ app.js
@@ -6,11 +6,16 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 const session = require('express-session');
+const MongoDBStore = require('connect-mongodb-session')(session);
 
 const errorController = require('./controllers/error');
 const User = require('./models/user');
 
 const app = express();
+const store = new MongoDBStore({
+  uri: process.env.DB_CONNECT,
+  collection: 'sessions'
+});
 
 app.set('view engine', 'ejs');
 app.set('views', 'views');
@@ -24,7 +29,8 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use(session({ 
     secret: process.env.SESSION_SECRET,
     resave: false,
-    saveUninitialized: false
+    saveUninitialized: false,
+    store: store
   }));
 
 app.use((req, res, next) => {
