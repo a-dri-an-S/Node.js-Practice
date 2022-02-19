999b83955d86383cbfa394f60038d14d8af8c7be
ADD RIAN <adrian.salinas.1986@gmail.com>
2022-02-18 17:13:16 -0800

    setting a cookie

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,12 +1,13 @@
 exports.getLogin = (req, res, next) => {
+    const loggedIn = req.get('Cookie').split(';')[2].split("=")[1];
     res.render('auth/login', {
         path: '/login',
         pageTitle: 'Login',
-        isAuthenticated: req.isLoggedIn
+        isAuthenticated: loggedIn
     });
 }
 
 exports.postLogin = (req, res, next) => {
-    req.isLoggedIn = true;
+    res.setHeader('Set-Cookie', 'loggedIn=true')
     res.redirect('/');
 }
