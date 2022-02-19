52ca4089351092f944949829581a9923f2c57005
ADD RIAN <adrian.salinas.1986@gmail.com>
2022-02-18 18:13:54 -0800

    added session middleware for storing session on server

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,13 +1,14 @@
 exports.getLogin = (req, res, next) => {
-    const loggedIn = req.get('Cookie').split(';')[2].split("=")[1];
+    // const loggedIn = req.get('Cookie').split(';')[2].split("=")[1];
+    console.log(req.session.isLoggedIn);
     res.render('auth/login', {
         path: '/login',
         pageTitle: 'Login',
-        isAuthenticated: loggedIn
+        isAuthenticated: false
     });
 }
 
 exports.postLogin = (req, res, next) => {
-    res.setHeader('Set-Cookie', 'loggedIn=true');
+    req.session.isLoggedIn = true;
     res.redirect('/');
 }
