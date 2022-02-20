30fbeee06fa6c8ba7fc575417d980de8593dcc73
ADD RIAN <adrian.salinas.1986@gmail.com>
2022-02-20 15:49:55 -0800

    updated files

--- controllers/auth.js
+++ controllers/auth.js
@@ -3,14 +3,20 @@ const User = require('../models/user');
 require('dotenv').config();
 
 exports.getLogin = (req, res, next) => {
-    // const loggedIn = req.get('Cookie').split(';')[2].split("=")[1];
-    console.log(req.session.isLoggedIn);
     res.render('auth/login', {
         path: '/login',
         pageTitle: 'Login',
         isAuthenticated: false
     });
-}
+};
+
+exports.getSignup = (req, res, next) => {
+    res.render('auth/signup', {
+        path: '/signup',
+        pageTitle: 'Signup',
+        isAuthenticated: false
+    });
+};
 
 exports.postLogin = (req, res, next) => {
     User.findById(process.env.USER_ID)
@@ -23,11 +29,14 @@ exports.postLogin = (req, res, next) => {
             });
         })
         .catch(err => console.log(err));
-}
+};
+
+exports.postSignup = (req, res, next) => { };
+
 
 exports.postLogout = (req, res, next) => {
     req.session.destroy((err) => {
         console.log(err);
         res.redirect('/')
     });
-}
+};
