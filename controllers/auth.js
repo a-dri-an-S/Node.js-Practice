990eeb8ac1fb73685ffe4e47f54b61974b9f6670
ADD RIAN <adrian.salinas.1986@gmail.com>
2022-02-21 11:52:02 -0800

    installed express-validator, refactored postLogin auth controller +  route for email validation

--- controllers/auth.js
+++ controllers/auth.js
@@ -3,6 +3,8 @@ const crypto = require('crypto');
 const bcrypt = require('bcryptjs');
 const nodemailer = require('nodemailer');
 const sendgridTransport = require('nodemailer-sendgrid-transport');
+const { validationResult } = require('express-validator');
+
 
 require('dotenv').config();
 
@@ -80,6 +82,16 @@ exports.postSignup = (req, res, next) => {
     const email = req.body.email;
     const password = req.body.password;
     const confirmPassword = req.body.confirmPassword;
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+        console.log(errors.array());
+        return res.status(422).
+            render('auth/signup', {
+                path: '/signup',
+                pageTitle: 'Signup',
+                errorMessage: errors.array()
+            });
+    }
 
     User.findOne({ email: email })
         .then(userDoc => {
