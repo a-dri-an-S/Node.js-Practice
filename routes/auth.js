db3073c0032311ca370452c400199fa1e57b9750
ADD RIAN <adrian.salinas.1986@gmail.com>
2022-02-21 12:11:35 -0800

    refactored post 'auth/signup' route for password validation

--- routes/auth.js
+++ routes/auth.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const { check } = require('express-validator');
+const { check, body } = require('express-validator');
 
 const authController = require('../controllers/auth');
 
@@ -11,7 +11,26 @@ router.get('/signup', authController.getSignup);
 
 router.post('/login', authController.postLogin);
 
-router.post('/signup', check('email').isEmail().withMessage('Please enter a valid email.'), authController.postSignup);
+router.post(
+    '/signup',
+    [
+        check('email')
+            .isEmail()
+            .withMessage('Please enter a valid email.')
+            .custom((value, { req }) => {
+                if (value === 'test@test.com') {
+                    throw new Error('This email address is forbidden!')
+                }
+                return true;
+            }),
+        body(
+            'password',
+            'Please enter a password that has numbers, letters and min length of 6 characters'
+            )
+            .isLength({ min: 6 })
+            .isAlphanumeric()
+    ],
+    authController.postSignup);
 
 router.post('/logout', authController.postLogout);
 
