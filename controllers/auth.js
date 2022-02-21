f879652870e184dc23fd9a48d58d03eb708c1499
ADD RIAN <adrian.salinas.1986@gmail.com>
2022-02-20 20:13:06 -0800

    installed nodemailer + noremailer transporter, refactored postSignup to send email if account creation successful

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,6 +1,16 @@
 const bcrypt = require('bcryptjs');
+const nodemailer = require('nodemailer');
+const sendgridTransport = require('nodemailer-sendgrid-transport');
+
+require('dotenv').config();
 
 const User = require('../models/user');
+
+const transporter = nodemailer.createTransport(sendgridTransport({
+    auth: {
+        api_key: process.env.SENDGRID_API_KEY
+    }
+}));
 
 require('dotenv').config();
 
@@ -86,7 +96,14 @@ exports.postSignup = (req, res, next) => {
                 })
                 .then(result => {
                     res.redirect('/login');
+                    return transporter.sendMail({
+                        to: email,
+                        from: 'augmentedrhythm@gmail.com',
+                        subject: 'Signup Successful!',
+                        html: '<h1>You successfully signed up!</h1>'
+                    })
                 })
+                .catch(err => console.log(err));
         })
         .catch(err => console.log(err));
 };
