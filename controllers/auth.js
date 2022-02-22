8f8004e66f0647a423e3aa57c10dc3f015f36d2a
ADD RIAN <adrian.salinas.1986@gmail.com>
2022-02-21 16:07:28 -0800

    added oldInput data to postSingup/ getSingup auth controllers to render, re-render user input to view if failed

--- controllers/auth.js
+++ controllers/auth.js
@@ -42,7 +42,12 @@ exports.getSignup = (req, res, next) => {
     res.render('auth/signup', {
         path: '/signup',
         pageTitle: 'Signup',
-        errorMessage: message
+        errorMessage: message,
+        oldInput: { 
+            email: "", 
+            password: "", 
+            confirmPassword: "" 
+        }
     });
 };
 
@@ -89,6 +94,7 @@ exports.postLogin = (req, res, next) => {
 exports.postSignup = (req, res, next) => {
     const email = req.body.email;
     const password = req.body.password;
+    const confirmPassword = req.body.confirmPassword;
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
         // console.log(errors.array());
@@ -96,7 +102,12 @@ exports.postSignup = (req, res, next) => {
             render('auth/signup', {
                 path: '/signup',
                 pageTitle: 'Signup',
-                errorMessage: errors.array()[0].msg
+                errorMessage: errors.array()[0].msg,
+                oldInput: { 
+                    email: email, 
+                    password: password, 
+                    confirmPassword: confirmPassword 
+                }
             });
     }
     bcrypt.hash(password, 12)
